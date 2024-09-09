75735e6f1f7c66cd9f4d044a3e98e2bc225d21fa
rbtsoheib <taharsoheib08@gmail.com>
2024-09-10 01:55:23 +0200

    new

--- techrs/src/App.js
+++ techrs/src/App.js
@@ -1,8 +1,11 @@
+import React from "react"
 import './App.css';
 
 function App() {
   return (
-    <></>
+    <>
+    
+    </>
   );
 }
 
--- techrs/src/components/home/fullpage/fullpage.jsx
+++ techrs/src/components/home/fullpage/fullpage.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-dom";
+import Navbar from "../navbar/Navbar"
 import "./fullpage.css";
 
 function fullpage() {
 
 return(
 
-    <></>
+    <>
+
+    <BrowserRouter>
+    <Navbar />
+    </BrowserRouter>
+    
+    </>
 
 );
 
